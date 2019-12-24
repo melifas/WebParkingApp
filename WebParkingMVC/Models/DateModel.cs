e9cdbd4582aeea04c3f9204d719a4e67a58febf3
melifas <meletisfasoulis@yahoo.gr>
2019-12-24 13:27:25 +0200

    Add MVC DateModel and View with jQueryCalendar

---
+++ WebParkingMVC/Models/DateModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebParkingMVC.Models
+{
+    public class DateModel
+    {
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+    }
+}
