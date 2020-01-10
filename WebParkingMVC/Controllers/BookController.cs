8f71d1c68829212f9c1ad734ba4f1ef742e796be
melifas <meletisfasoulis@yahoo.gr>
2020-01-10 22:57:52 +0200

    FirstCommit to the new photo branch

--- WebParkingMVC/Controllers/BookController.cs
+++ WebParkingMVC/Controllers/BookController.cs
@@ -34,33 +34,13 @@ namespace WebParkingMVC.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult BookRoom(BookRoomModel model)
-        {
-            /* if (ModelState.IsValid)
-             {
-                 bool status = false;
-                 //bool response = da.SaveBooking(model);
-                 if (status)
-                 {
-                     Response.Redirect("~/");
-                 }
-                 else
-                 {
-                     ModelState.AddModelError("", "Cannot save to database");
-                 }*/
+        {           
             if (ModelState.IsValid)
             {
                 da.BookClient(model.FirstName, model.LastName, model.startDate, model.endDate, model.ParkingtypeId);                
                 return RedirectToAction("Index");
             }
-            return View(model);
-
-
-        
-
-       /* IEnumerable<SelectListItem> ParkingsTypeList = da.getParkingsType().Select(m => new SelectListItem() { Text = m.Title, Value = m.Id.ToString() }).ToArray();
-            ViewBag.ParkingsTypes = ParkingsTypeList;
-
-            return View(model);*/
+            return View(model);              
         }
 
 
@@ -73,28 +53,6 @@ namespace WebParkingMVC.Controllers
             }
 
             return View();
-        }
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //[Route("Book/CreateBooking/{ParkingTypeId}")]
-        //public ActionResult CreateBooking(string firstName, string lastName, DateTime? startDate, DateTime? endDate, int? ParkingTypeId)
-        //{
-
-        /* BookRoomModel book = new BookRoomModel();
-         book.FirstName = firstName;
-         book.LastName = lastName;
-         book.startDate = startDate;
-         book.endDate = endDate;
-         book.ParkingtypeId = ParkingTypeId;
-         if (ModelState.IsValid)
-         {
-             Data_Access da = new Data_Access();
-             da.BookClient(firstName, lastName, startDate, endDate, ParkingTypeId);
-             return RedirectToAction("Index");
-         }*/
-        //return View();
-
-        //}
+        }       
     }
 }
