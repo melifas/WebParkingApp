33fed979dbc23310e86a8d6adaa74cf1f002da96
melifas <meletisfasoulis@yahoo.gr>
2020-01-10 23:00:24 +0200

    Db commited in the project

--- WebParkingDB/dbo/Tables/Clients.sql
+++ WebParkingDB/dbo/Tables/Clients.sql
@@ -2,9 +2,11 @@
     [Id]        INT           IDENTITY (1, 1) NOT NULL,
     [FirstName] NVARCHAR (50) NOT NULL,
     [LatName]   NVARCHAR (50) NOT NULL,
-    [Phone]     INT           NOT NULL,
-    [Email]     NVARCHAR (50) NOT NULL,
+    [Phone]     INT           NULL,
+    [Email]     NVARCHAR (50) NULL,
     CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
