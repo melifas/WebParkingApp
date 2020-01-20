cde1ca8f21d02f746f76d4ee24deb76dc4c3eaa7
melifas <melifas@DESKTOP-QTIDJ0F>
2020-01-20 15:11:16 +0200

    Database Updated

--- WebParkingDB/dbo/Stored Procedures/spgetAvailableparkingsType.sql
+++ WebParkingDB/dbo/Stored Procedures/spgetAvailableparkingsType.sql
@@ -1,5 +1,5 @@
 ﻿-- ================================================
-CREATE PROCEDURE dbo.[spgetAvailableparkingsType]
+CREATE PROCEDURE [dbo].[spgetAvailableparkingsType]
 @startDate date,
 @endDate date
 
@@ -9,7 +9,7 @@ begin
 
 set nocount on;
 /****** Εμφάνιση ένωσης πινάκων  ******/
-select t.Id,t.Title,t.Desription,t.Price	
+select t.Id,t.Title,t.Desription,t.Price,COUNT(Title) AS remainingPositions	
 from dbo.Parkings p
 inner join dbo.ParkingTypes t on t.Id=p.ParkingTypeId
 /****** συνέχιση του query για αυτά που δεν είναι booked  ******/
