2bd02d34dc0f70736a5e85a0662c0683e18adacc
melifas <melifas@DESKTOP-QTIDJ0F>
2020-02-04 17:08:59 +0200

    Modifications to Table Columns

---
+++ LibraryWebParking/ModelMetadata/spParkingWithTypes_Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryWebParking.Model
+{
+    [MetadataType(typeof(spgetAvailableparkingsTypeCount_metadata))]
+    public partial class spParkingWithTypes_Result
+    {
+        internal class spgetAvailableparkingsTypeCount_metadata
+        {
+            [Display(Name ="Parking Number")]
+            public string ParkingNumber { get; set; }
+            [Display(Name ="Type of Car")]
+            public string Title { get; set; }
+            public int Id { get; set; }
+            public int ParkingTypeId { get; set; }
+            public string Desription { get; set; }
+            public decimal Price { get; set; }
+
+        }
+    }
+}
