9ffaf5ddc9ce2c1cf83f4a3d4a4d58c1be0f27e9
melifas <melifas@DESKTOP-QTIDJ0F>
2020-02-27 20:18:03 +0200

    Html Page Class Problem fixed

--- WebParkingMVC/App_Start/BundleConfig.cs
+++ WebParkingMVC/App_Start/BundleConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
-namespace Inspinia_MVC5
+namespace WebParkingMVC
 {
     public class BundleConfig
     {
--- WebParkingMVC/Global.asax.cs
+++ WebParkingMVC/Global.asax.cs
@@ -1,4 +1,4 @@
-using Inspinia_MVC5;
+using WebParkingMVC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
